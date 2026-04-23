e2f5c6cd774e03eb0f03211d5d0fb910de7c79d8
dleeftink <dennisleeftink@gmail.com>
2026-04-23 22:24:05 +1000

    Create lay/meta.sql

---
+++ bq/lay/meta.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE FUNCTION lay.meta() AS (STRUCT(
+  "Layers: Formatters and layouters for the 'substep' namespace." AS scope,
+  "v0.0.0" AS version,
+  null AS repo
+)) OPTIONS (
+  description = "Formatters and layouters for the 'substep' namespace."
+);
