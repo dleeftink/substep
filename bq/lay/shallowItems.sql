780cd68b74dbb5b9e58cec49767309f9f774ab8e
dleeftink <dennisleeftink@gmail.com>
2026-04-26 14:53:59 +0000

    Update description in shallowItems function for clarity

--- bq/lay/shallowItems.sql
+++ bq/lay/shallowItems.sql
@@ -1,5 +1,5 @@
 create or replace function lay.shallowItems(jsn STRING) as (
   (jsn).replace('"#":{','').replace('},"#":#','')
 ) OPTIONS (
-  description = "Unescapes temporarily nested JSON key/value pairs."
+  description = "Removes temporary object boundaries from nested JSON items."
 );
