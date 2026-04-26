8f4c4c8543145a719846af8be392650832ed717f
dleeftink <dennisleeftink@gmail.com>
2026-04-26 15:02:35 +0000

    Create get/keyFragment.sql

---
+++ bq/get/keyFragment.sql
@@ -0,0 +1,5 @@
+create or replace function get.keyFragment(jsn STRING,open INT, keypos INT) as (
+  (jsn).substring(keypos+1,greatest(0,open-keypos-0)).ltrim(' ') 
+) OPTIONS (
+  description = "Extracts a JSON key segment based on open and closing positions."
+);
