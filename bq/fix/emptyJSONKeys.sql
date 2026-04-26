24c85fa729b32002f50fd40d97589a54e164f544
dleeftink <dennisleeftink@gmail.com>
2026-04-26 15:13:14 +0000

    Create fix/emptyJSONKeys.sql

---
+++ bq/fix/emptyJSONKeys.sql
@@ -0,0 +1,9 @@
+create or replace function fix.emptyJSONKeys(jsn STRING) as (
+  (jsn)
+    .regexp_replace(r'""\:([^\{\}\[\]]*?)\,""\:([^\{\}\[\]]*?)',r'\1:\2')  -- move quoted keys/values into empty key position and mark insertion point
+    .regexp_replace(r'([\{\,])\s*([^"\:\{\[\]\}\s]+)(\s*\:)',  r'\1"\2"\3') -- handle unquoted keys
+    --.regexp_replace(r'^\{"":\{','{"_root":{') -- set root in case of unnamed top-level struct
+    .replace('"":','"undefined":')
+) OPTIONS (
+  description = "Resolves empty:non-empty key/value sequences resulting from SQL-to-JSON conversion."
+);
