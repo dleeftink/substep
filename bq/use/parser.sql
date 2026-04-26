96e2292c3b7553317dd457468f7539556dde2d85
dleeftink <dennisleeftink@gmail.com>
2026-04-26 15:20:42 +0000

    Create use/parser.sql

---
+++ bq/use/parser.sql
@@ -0,0 +1,23 @@
+create or replace function use.parser(object ANY TYPE, maxDepth INT) as ((
+
+  with safe as (
+    select get.safeJSON(object) as jsn,[('[',']'),('{','}')] as pairs
+  ),
+
+  main as (
+
+    select jsn as str,array(
+      select as struct * replace(array(
+        select as struct * replace((json).(lay.unsafeJSON)().(safe.parse_json)() as json) 
+        from unnest(level.children)) as children
+      ) from unnest(get.unrolled(jsn,pairs,maxDepth)) level
+    ) levels, (jsn).starts_with('[{') or (jsn).starts_with('[[') is_array_root 
+    from safe
+
+  )
+
+  select as struct * from main
+
+)) OPTIONS (
+   description = "Parses a complex SQL object into plain JSON by canonicalising the input."
+);
