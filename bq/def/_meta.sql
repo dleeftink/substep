7ea8f14fc441279924c4c86edbd8a76c8fe13c1d
dleeftink <dennisleeftink@gmail.com>
2026-04-26 15:46:13 +0000

    Bump all namespace versions to 0.1.0 for initial release

--- bq/def/_meta.sql
+++ bq/def/_meta.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE FUNCTION def.meta() AS (STRUCT(
   "Contracts: Schema defaults and API definitions for the 'substep' namespace." AS scope,
-  "v0.0.0" AS version,
+  "0.1.0" AS version,
   "https://github.com/dleeftink/substep" AS repo
 )) OPTIONS (
   description = "Schema defaults and API definitions for the 'substep' namespace."
--- bq/get/_meta.sql
+++ bq/get/_meta.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE FUNCTION get.meta() AS (STRUCT(
   "Extraction: Parsers and extractors for the 'substep' namespace." AS scope,
-  "v0.0.0" AS version,
+  "0.1.0" AS version,
   "https://github.com/dleeftink/substep" AS repo
 )) OPTIONS (
   description = "Parsers and extractors for the 'substep' namespace."
