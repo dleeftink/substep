af89f8104d0a29b86751d7e02b5e0edd958db8c8
dleeftink <dennisleeftink@gmail.com>
2026-04-26 15:08:23 +0000

    Create cue/objectMetadataInterface.sql

---
+++ bq/cue/objectMetadataInterface.sql
@@ -0,0 +1,9 @@
+create or replace function cue.objectMetadataInterface(a INT, b INT, jsn STRING, head ANY TYPE, tail ANY TYPE, slot INT, kpos INT) as (
+  struct(
+    head.raise,b as depth,slot,a as pre,head.idx as open,tail.idx as close,kpos,head.nest,
+    get.keyFragment(jsn,head.idx,kpos) as key,null as arr_sym,null as arr_ctx,null as ord,null as sym, 
+    get.objectFragment(jsn,head.idx,tail.idx) as json, null as acid,null as ocid,null as ecid,false as list --> acid: array container id / ocid: object container id / ecid: element container id
+  )
+) OPTIONS (
+  description = "Defines the internal `get.objectMetadata()` interface."
+);
