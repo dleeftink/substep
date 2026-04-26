a5a17b4b74a2b9a639decc7e3262b73671822ad4
dleeftink <dennisleeftink@gmail.com>
2026-04-26 14:53:33 +0000

    Update description in shallowItems function for clarity

--- bq/fix/shallowItems.sql
+++ bq/fix/shallowItems.sql
@@ -1,5 +1,5 @@
 create or replace function fix.shallowItems(jsn STRING) as (
   (jsn).regexp_replace(r'((?:"[^"]*"|[^:,{}]*)\s*:\s*(?:"[^"]*"|[\d\.]+|true|false|null))',r'"#":{\1},"#":#')
 ) OPTIONS (
-  description = "Temporarily escapes shallow JSON key/value pairs."
+  description = "Wraps shallow JSON key/value pairs with temporary object boundaries."
 );
