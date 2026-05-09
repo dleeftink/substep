150329462d06534985674e13796d8d24bad2d3ae
dleeftink <dennisleeftink@gmail.com>
2026-05-09 10:20:51 +0000

    Create session(26.05.09)queries(0.1)json(cast)mvp(0.1).sql

---
+++ bq/try/session(26.05.09)queries(0.1)json(cast)mvp(0.1).sql
@@ -0,0 +1,75 @@
+-- Generate signatures strategy F (direct array of structs access):
+-- requires two different function calls depending on source type (struct or array)
+
+create or replace function tmp.getJsonSigFromStruct(blob any type) as (
+ (select as struct [(blob).to_json()] parts,farm_fingerprint(str) sig,length(str) rel,'object' type from (select safe.format('%t',blob) str))
+);
+
+create or replace function tmp.getJsonSigFromArray(blob any type,tail int,scan int) as ((
+  select as struct array((
+    select (b).to_json() from (select b,i from unnest(blob) b with offset i) where if(tail is null,true,i > len - 2 - tail )
+    )) parts, farm_fingerprint(str) sig,length(str) rel,'array' type from (
+      select if(scan=0,
+        safe.format('%t',blob[0])||safe.format('%t',array_last(blob)),
+
+        (select if(a=b,a,a||b) sttr from (
+        select
+          array_to_string(array(select safe.format('%t',blob[safe_offset(idx)]).ifnull('') from unnest(indx) idx),'') a ,
+          array_to_string(array(select safe.format('%t',blob[safe_offset(array_length(blob)-1-idx)]).ifnull('') from unnest(indx) idx),'')  b
+          from (select generate_array(0,scan) indx)
+        ))
+    ) as str,array_length(blob) len
+  )
+
+));
+
+
+create or replace table function tmp.useJsonSchemaSampler4(input table<sig int,jsn json>) as (
+from input |> select sig |> limit 1
+  |> select sig
+  
+);
+
+create or replace table function tmp.getJsonPathsThru4(input table<sig int64,jsn json>,constants any type) as (
+
+  -- A: closure pattern
+  select sig,array_agg(struct((jsn).to_json_string().length()  as jsn, constants as c)) parts from input
+  group by sig
+
+  -- B: broadcast pattern
+  -- select sig,array_agg(struct((jsn).to_json_string().length()  as jsn,c)) parts from input
+  -- cross join unnest([constants]) c group by sig
+
+);
+
+create or replace table function tmp.getJsonPaths4(input table<src struct<parts array<json>,sig int,rel int,type string>>) as (
+
+  with init as (
+     
+    select src.sig,jsn from input get,get.src.parts jsn
+    
+  ),
+  
+  exit as (
+    select * from tmp.getJsonPathsThru4(table init,
+      constants => (from init |> call tmp.useJsonSchemaSampler4())
+    )
+  )
+
+  select * from exit
+
+);
+
+
+with real as (
+ 
+  select tmp.getJsonSigFromArray(blob,tail=>1,scan=>0) src from (
+    select
+      hits as blob
+    from (
+      select * from `stack-curves.tables.hits` -- limit 15
+    ) --get,get.hits hit
+  )
+)
+
+select * from tmp.getJsonPaths4(table real)
