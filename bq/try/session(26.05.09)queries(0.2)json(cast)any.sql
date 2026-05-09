9c0cd093840c647f56a2e87516003195fa926232
dleeftink <dennisleeftink@gmail.com>
2026-05-09 08:54:03 +0000

    Create session(26.05.09)queries(0.2)json(cast)any.sql

--- bq/try/session(26.05.09)queries(0.2)json(cast)any.sql
+++ bq/try/session(26.05.09)queries(0.2)json(cast)any.sql
@@ -1,46 +1,21 @@
--- Generate signatures strategy F (direct array of structs access):
-create or replace function tmp.getJsonSigFromStruct(blob any type) as (
- (select as struct [(blob).to_json()] parts,farm_fingerprint(str) sig,length(str) rel,'object' type from (select safe.format('%t',blob) str))
-);
+-- Generate signatures strategy G (direct any struct > JSON access):
 
-create or replace function tmp.getJsonSigFromArray(blob any type,tail int,scan int) as ((
-  select as struct array((
-    select (b).to_json() from (select b,i from unnest(blob) b with offset i) where if(tail is null,true,i > len - 2 - tail )
-    )) parts, farm_fingerprint(str) sig,length(str) rel,'array' type from (
-      select if(scan=0,
-        --if(take is null,safe.format('%t',blob),safe.format('%t',blob[0])||safe.format('%t',array_last(blob))) str
-        safe.format('%t',blob[0])||safe.format('%t',array_last(blob)),
-
-        (select if(a=b,a,a||b) sttr from (
-        select
-          array_to_string(array(select safe.format('%t',blob[safe_offset(idx)]).ifnull('') from unnest(indx) idx),'') a ,
-          array_to_string(array(select safe.format('%t',blob[safe_offset(array_length(blob)-1-idx)]).ifnull('') from unnest(indx) idx),'')  b
-          from (select generate_array(0,scan) indx)
-        ))
-    ) as str,array_length(blob) len
-  )
-
-    
-
-    /*(select as struct array_agg((obj).to_json()) parts,bit_xor(farm_fingerprint(str)) sig,sum(length(str)) rel,'array' type from (
-      select *, safe.format('%t',obj) str from (
-        select obj,idx from unnest(blob) obj with offset idx)) where if(take is null,true,i < (take)),*/
-    
-   
-
-
-  -- if(jsn[0] is not null,
-  --   (select as struct jsn[0] jsn,farm_fingerprint((jsn[0]).to_json_string()||array_last(json_query_array(jsn)).to_json_string()) sig),
-  --   (select as struct jsn,farm_fingerprint((jsn).to_json_string()) sig)
-  -- )
+create or replace function tmp.getJsonSigFromAny1(blob any type,tail int,scan int) as ((
+  select if(jsn[0] is not null,
   
-  --(select as struct (sel).ifnull(jsn) jsn, if(sel is null,'object','array') type from (select jsn[0] sel))
-  --struct( (jsn[0]).ifnull(jsn) as jsn, if(jsn[0] is null,'object','array') as type)
-  --(select if(src.jsn is not null,src,null) from (select struct(jsn[0] as jsn, 'array' as type) src)).ifnull(struct(jsn,'object'))
-  --if(jsn[0] is not null,struct(jsn[0],'array' as type),struct(jsn,'object' as type))
-  --from (
-  --  select (blob).to_json() jsn
-  --)
+    --(select as struct array(select * from unnest(jsns) jsn with offset idx |> where idx < 1 |> select jsn) jsns from (select json_query_array(jsn) jsns)),
+    (select as struct array_agg(if(incTail,json 'null',null) ignore nulls) parts,bit_xor(if(incScan,farm_fingerprint(str),null)) sig,sum(if(incTail,length(str),null)) rel,'array' type from (
+        select *,tail is null or i > len - 2 - tail incTail, scan is null or i not between scan and len - 1 - scan or (i = 0 or i = len -1) incScan from (
+          select i,obj,safe.format('%t',obj) str,array_length(jsns) len, 
+          from ( select json_query_array(jsn) jsns) get,get.jsns obj with offset i)
+        )
+      ),
+  
+    (select as struct [jsn] parts,farm_fingerprint(str) sig,length(str) rel,'object' type from (select safe.format('%t',blob) str))
+  ) jsn
+  from (
+    select (blob).to_json() jsn
+  )
 ));
 
 /*create or replace table function tmp.getJsonPathsSort3(input table<src struct<jsn json,str string>>) as (
@@ -60,7 +35,7 @@ create or replace function tmp.getJsonSigFromArray(blob any type,tail int,scan i
 
 with real as (
  
-  select tmp.getJsonSigFromArray(blob,tail=>0,scan=>0) from (
+  select tmp.getJsonSigFromAny1(blob,tail=>0,scan=>0) from (
     select
       hits as blob
     from (
