9b7b1aa7f49be16d3e254883ee09b8e3f266a1e9
dleeftink <dennisleeftink@gmail.com>
2026-05-13 08:34:30 +0000

    Create session(26.05.12)queries(0.3)flat(json)values(reduced)aggs.sql

---
+++ bq/try/session(26.05.12)queries(0.3)flat(json)values(reduced)aggs.sql
@@ -0,0 +1,127 @@
+-- reduces the amount of aggregations in the scalar nest CTE
+-- additionally pre-qualifies data with a quick signature check in json CTE
+
+create temp function getJsonIndex(str string,rgx string) as (array(
+  with init as (
+    select /*(SUM(LENGTH(sub)) OVER (ORDER BY off ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING) + 1 ).ifnull(0)*/ off idx, sub,right(sub,1) tail
+    from unnest(regexp_extract_all(str,rgx)) AS sub WITH OFFSET AS off
+  )
+
+  select as struct idx,(
+    case
+    when tail in ('[',']') then struct(tail as mark,'ARRAY' as type,(sub).rtrim(':[').replace('""','"undefined"') as item)
+    when tail in ('{','}') then struct(tail as mark,'OBJECT' as type,(sub).rtrim(':{') as item)
+    else struct(':' as mark, 'ENTRY' as type,sub as item)
+    end
+  ).* from init where tail not in (',',' ')
+));
+
+with test as (
+
+  select typeof(blob) type,blob,sel from unnest([struct([
+    struct(1 as `id asdasd`, [struct(r'a[]\'s' as sub,2 as val,[struct(1.123 as x,2 as y),(1.0,2),(9.0,0)] as nest),("b",5, [(2.0,3)])] /* as arr */,1 as uid,'x" a' as sub), 
+    (2, [("c,a",4,[(3.0,4)]),("d",5,[(5.0,6)])],2,'y STRUCT<>')
+    ] as blob,1 as sel),
+    struct([
+    (3, [("c",4,[(3.0,4)]),("d",5,[(5.0,6)])],2,'x'),
+    (4, [("c",4,[(3.0,4)]),("d",5,[(5.0,6)])],2,'y')
+    ],0 as sel)
+  ])
+),
+
+-- processing gets prohibitively expensive for real data
+
+real as (
+ 
+  select typeof(blob) type,blob,null as sel from (
+    select
+      hits as blob
+    from (
+      select * from `stack-curves.tables.hits` -- limit 512
+    ) -- get,get.hits hit -- limit 1
+  )
+),
+
+json as (
+
+  select str,(str).replace('\\"','\x05').getJsonIndex(
+  ('('
+    ||[
+    r'"[^"]*"\s*:\s*(?:"[^"]*"|[\d\.]+|true|false|null|[\[\{])', -- primitive fields + named structures
+    -- r'"[^"]*"\s*:\s*[\[\{]', -- named structures
+    r'[\[\]\{\}]', -- structural markers
+    r'\,\s*'
+    --r'[^\'"\[\]\{\}, ]+' -- remaining values (split)
+    
+    ].array_to_string('|')
+    ||
+  ')')
+  ) index from (
+    
+    -- this reliably triggers a shuffle across slots
+    select (blob).to_json_string() str from real
+    qualify row_number() over(partition by farm_fingerprint(safe.format('%t',(blob).array_last()))) = 1
+  
+  )
+
+),
+
+nest as (
+
+  select str,array(
+    from unnest(index) with offset as temp_slot
+    |> extend mark in ('{','[') as opener, mark in (']','}') as closer,type in ('ENTRY') as entry
+    |> extend sum(case when opener then 1 when closer then -1 else 0 end) over(w1) - 1 as depth
+       window w1 as (order by idx rows between unbounded preceding and current row)
+
+    |> extend depth - (case when opener then 1 when closer then -1 else 0 end) as pre
+    |> extend if(entry,1,0) as lift
+
+    |> select pre,depth,* except(pre,depth)
+    |> set pre = pre + lift , depth = depth + lift
+
+    |> where pre < /*pick*/10 + 1 
+    |> extend pre > depth as pin
+    |> set depth =  if(pre > depth,pre,depth)
+    -- don't need this
+    -- |> aggregate array_agg(struct(pin,idx,mark,type,item,entry,closer) /*order by idx*/) subs
+    --     group by depth --pre a,depth b 
+    --|> cross join unnest(subs) obj with offset as slot 
+
+    -- may need this
+    --|> extend row_number() over(/*partition by depth order by idx*/) slot
+    |> extend temp_slot as slot
+    |> as obj
+    |> aggregate min_by(obj,pin) head,max_by(obj,pin) tail group by depth,slot - if(closer,1,0) as slot -- if(entry,item,type)
+
+    |> cross join unnest(generate_array(0,1)) raise
+    |> where not (raise = 1 and head.entry)
+    |> set depth = depth + raise,raise = if(raise = 0,true,false) 
+    |> select 
+        raise,depth,slot,head.idx as open,tail.idx + if(head.entry,length(head.item).ifnull(0),0) as close,
+        head.mark head,head.type,head.item as data,tail.mark tail,--null as part
+
+    -- |> set data = coalesce(substring(str,open,close-open).left(16).concat('...')) -- check if correct index
+    |> as obj
+    |> aggregate
+        array_agg(if(raise,obj,null) ignore nulls /*order by obj.open*/) children,
+        -- array_agg(if(not raise and type in ("ARRAY","OBJECT"),obj,null) ignore nulls order by obj.open) parents,
+        -- array_agg(if(not raise and type in ("STRUCT","ARRAY"),obj.close,null) ignore nulls order by obj.open) looks 
+      group by depth
+   
+    |> where array_length(children) > 0   
+    |> select as struct array_length(children) len,*,
+    |> select as struct *
+    -- |> order by depth
+
+  ) as levels from json 
+
+)
+
+--select getJsonBlobSigFromArray(blob,type,0,0).sig from real group by sig
+-- select sig,(levels).array_last().children.array_last().data
+--   from nest
+
+select (levels).array_last().children.array_last().data 
+ from nest
+
